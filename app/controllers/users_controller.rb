8e6bb67608eb734395098ddd309536b8946e53fa
veryamo <zhestochok@gmail.com>
2017-02-15 20:21:27 +0900

    Finish user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url
+      log_in @user
+      redirect_to @user
       flash[:success]="Добро пожаловать!"
     else
       render 'new'
