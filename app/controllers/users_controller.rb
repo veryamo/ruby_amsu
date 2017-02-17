3b00cd4cefcdc40497a9a4a8549ba3f7c3d4b5ff
veryamo <zhestochok@gmail.com>
2017-02-17 16:32:44 +0900

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :logged_in_user, only: [:edit, :update]
+before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -18,6 +20,29 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user 
+    else
+      render 'edit'
+    end
+  end
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please, log in, before this action"
+        redirect_to login_url
+      end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
   private
   def user_params
