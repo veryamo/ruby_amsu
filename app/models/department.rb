ea3191956a70d83064b4af3b3842dc6cbecd1911
veryamo <zhestochok@gmail.com>
2018-04-02 23:20:15 +0900

    finally fixed self-reference on departments

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,7 @@
 class Department < ApplicationRecord
     has_many :individuals
-    belongs_to :parental_dept, class_name: 'Department', polymorphic: true, validate: false, optional: true
-    has_many :child_depts, class_name: 'Department', foreign_key: 'parental_dept_id', validate: false
+    belongs_to :parental_dept, class_name: 'Department', optional: true
+    has_many :child_depts, class_name: 'Department', foreign_key: 'parental_dept_id'
     def parental_dept_name
         parental_dept.try(:name)
     end
